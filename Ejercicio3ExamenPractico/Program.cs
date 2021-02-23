3e51b2f9bf376b5df32f968a4c05ddbe8bba1782
Yaser.AndOr <yaser1222@hotmail.com>
2021-02-22 20:57:03 -0600

    Ejercicio3

---
+++ Ejercicio3ExamenPractico/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio3ExamenPractico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Convertir el Diagrama de flujos a código C#
+
+            double nota1, nota2, nota3, promedio;
+            Console.WriteLine("Ingrese el valor de la primera nota");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de la segunda nota");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de la tercera nota");
+            nota3 = double.Parse(Console.ReadLine());
+            //Se realiza la suma de las 3 notas y luego se divide entre 3 para obtener el promedio
+            promedio = (nota1 + nota2 + nota3) / 3;
+            //Si la nota promedio es mayor o igual que 7, significa que ha sido promocionado
+            if (promedio >= 7) 
+            {
+                Console.WriteLine("Felicidades, has sido promocionado con una nota de " + promedio);
+            }
+            //Si la nota promedio es mayor o igual que 4, significa que obtuvo nota regular pero ha sido reprobado
+            else if (promedio >= 4) 
+            {
+                Console.WriteLine("Ánimo, tú puedes lograrlo, tu promedio ha sido de " + promedio + " , nota regular pero has reprobado la materia");
+            }
+            //Si la nota promedio es menor que 4, significa que obtuvo una nota muy baja e igualmente ha sido reprobado
+            else if (promedio < 4) 
+            {
+                Console.WriteLine("Vamos tú puedes hacerlo, debes esforzarte más, tu promedio ha sido de " + promedio + ", has sido reprobado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio3ExamenPractico/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Ejercicio3ExamenPractico")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Ejercicio3ExamenPractico")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("0e4062d5-197d-4336-b9fd-a2824822e560")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
